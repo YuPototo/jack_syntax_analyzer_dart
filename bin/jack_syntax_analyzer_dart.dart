04aeb16ece3ba755ae00ca0e612bee30b4dcc61d
Yu Qin <sirius.qinyu@gmail.com>
2024-01-02 14:28:58 +0900

    init

---
+++ bin/jack_syntax_analyzer_dart.dart
@@ -0,0 +1,5 @@
+import 'package:jack_syntax_analyzer_dart/jack_syntax_analyzer_dart.dart' as jack_syntax_analyzer_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${jack_syntax_analyzer_dart.calculate()}!');
+}
---
+++ test/jack_syntax_analyzer_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:jack_syntax_analyzer_dart/jack_syntax_analyzer_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
