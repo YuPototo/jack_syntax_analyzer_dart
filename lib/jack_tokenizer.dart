6b6f6565eeaa903b4c254eaeac4d024c25c650e6
Yu Qin <sirius.qinyu@gmail.com>
2024-01-07 13:14:59 +0900

    Finish has more impl

--- lib/jack_tokenizer.dart
+++ lib/jack_tokenizer.dart
@@ -1,3 +1,8 @@
+enum CommentType {
+  doubleSlash, // // comment \n
+  slashStar, // /* comment */ or /** comment */
+}
+
 class JackTokenizer {
   String scriptContent;
   int cursor; // index of currentToken's last character
@@ -7,5 +12,88 @@ class JackTokenizer {
   JackTokenizer(this.scriptContent) : cursor = -1;
 
   // A core assumption: the scriptContent is a valid Jack script.
-  bool hasMoreTokens() {}
+  bool hasMoreTokens() {
+    if (cursor + 1 >= scriptContent.length) {
+      return false;
+    }
+
+    int tmpCursor = cursor;
+
+    String possibleToken = "";
+    // get next character
+    bool getMoreCharacter = true;
+
+    while (getMoreCharacter) {
+      tmpCursor++;
+
+      if (tmpCursor >= scriptContent.length) {
+        return false;
+      }
+
+      String newChar = scriptContent[tmpCursor];
+
+      // identify the first valid character
+      if (possibleToken.isEmpty) {
+        // ignore whitespace and newlines
+        if (newChar == " " || newChar == "\n") {
+          continue;
+        } else if (newChar == '/') {
+          tmpCursor++;
+          String nextChar = scriptContent[tmpCursor];
+          if (nextChar == '/') {
+            while (scriptContent[tmpCursor] != '\n') {
+              tmpCursor++;
+              if (tmpCursor >= scriptContent.length) {
+                return false;
+              }
+            }
+          } else if (nextChar == '*') {
+            // This case would be invalid Jack script:
+            //    An open /* without closing */
+            // It would not happen to this project because
+            // we assume all jack files are valid
+            if (tmpCursor + 2 >= scriptContent.length) {
+              return false;
+            }
+
+            String possibleEndComment =
+                scriptContent[tmpCursor + 1] + scriptContent[tmpCursor + 2];
+
+            while (possibleEndComment != '*/') {
+              tmpCursor++;
+              if (tmpCursor + 2 >= scriptContent.length) {
+                return false;
+              }
+              possibleEndComment =
+                  scriptContent[tmpCursor + 1] + scriptContent[tmpCursor + 2];
+            }
+          } else {
+            // I don't think we will reach here
+            throw Exception("Invalid comment");
+          }
+        } else {
+          // if it is not a whitespace, newline and comment
+          // then it is a valid token, because we assume the script is valid
+          return true;
+        }
+      }
+    }
+  }
 }
+
+/**
+ * Possible way to get next token:
+ *   - get the first valid character
+ *   - if it is a comment, then continue to the end of the comment. 
+ *   -    try to find a valid token after the comment
+ *   - if it is a string, then continue to the end of the string. Return True.
+ *   - if it is a normal character, then do following check
+ *      - if it is a symbol, then return True
+ *      - else continue till the end of the token. Return True.
+ * 
+ * hasMoreTokens():
+ *    - get the first valid character, it should not be whitespace or newline
+ *    - if it is a comment, then continue to the end of the comment. 
+ *       - then start again
+ *    - else it is definitely true
+ */
--- test/jack_tokenizer_test.dart
+++ test/jack_tokenizer_test.dart
@@ -4,76 +4,118 @@ import 'package:jack_syntax_analyzer_dart/jack_tokenizer.dart';
 
 void main() {
   group('hasMoreTokens', () {
-    test("A simple true", () {
-      var scriptContent = 'let x = 5;';
-      var tokenizer = JackTokenizer(scriptContent);
-      expect(tokenizer.hasMoreTokens(), equals(true));
+    group('comment', () {
+      test('ignore // comment', () {
+        var scriptContent = '// comment';
+        var tokenizer = JackTokenizer(scriptContent);
+        expect(tokenizer.hasMoreTokens(), equals(false));
+      });
+
+      test('ignore // comment \n', () {
+        var scriptContent = '// comment \n';
+        var tokenizer = JackTokenizer(scriptContent);
+        expect(tokenizer.hasMoreTokens(), equals(false));
+      });
+
+      test('ignore /* comment */', () {
+        var scriptContent = '/* comment */';
+        var tokenizer = JackTokenizer(scriptContent);
+        expect(tokenizer.hasMoreTokens(), equals(false));
+      });
+
+      test('ignore /** comment */', () {
+        var scriptContent = '/* comment */';
+        var tokenizer = JackTokenizer(scriptContent);
+        expect(tokenizer.hasMoreTokens(), equals(false));
+      });
+
+      test('code after // comment', () {
+        var scriptContent = '// comment\nlet x = 5;';
+        var tokenizer = JackTokenizer(scriptContent);
+        expect(tokenizer.hasMoreTokens(), equals(true));
+      });
+
+      test('code after /* comment */', () {
+        var scriptContent = '/* comment */let x = 5;';
+        var tokenizer = JackTokenizer(scriptContent);
+        expect(tokenizer.hasMoreTokens(), equals(true));
+      });
+
+      test('code after /** comment */', () {
+        var scriptContent = '/** comment */let x = 5;';
+        var tokenizer = JackTokenizer(scriptContent);
+        expect(tokenizer.hasMoreTokens(), equals(true));
+      });
     });
 
-    test("A simple false", () {
-      var scriptContent = '';
-      var tokenizer = JackTokenizer(scriptContent);
-      expect(tokenizer.hasMoreTokens(), equals(false));
+    group('handle empty space and newlines', () {
+      test("A simple false", () {
+        var scriptContent = '';
+        var tokenizer = JackTokenizer(scriptContent);
+        expect(tokenizer.hasMoreTokens(), equals(false));
+      });
+
+      test('Ignore whitespace', () {
+        var scriptContent = '  ';
+        var tokenizer = JackTokenizer(scriptContent);
+        expect(tokenizer.hasMoreTokens(), equals(false));
+      });
+
+      test('Ignore newlines', () {
+        var scriptContent = '\n';
+        var tokenizer = JackTokenizer(scriptContent);
+        expect(tokenizer.hasMoreTokens(), equals(false));
+      });
+
+      test('Ignore whitespace and newlines', () {
+        var scriptContent = '  \n';
+        var tokenizer = JackTokenizer(scriptContent);
+        expect(tokenizer.hasMoreTokens(), equals(false));
+      });
+
+      test('Token after new line and whitespace', () {
+        var scriptContent = '  \n let x = 5;';
+        var tokenizer = JackTokenizer(scriptContent);
+        expect(tokenizer.hasMoreTokens(), equals(true));
+      });
     });
 
-    test('Ignore whitespace', () {
-      var scriptContent = '  ';
-      var tokenizer = JackTokenizer(scriptContent);
-      expect(tokenizer.hasMoreTokens(), equals(false));
-    });
+    group('has more token', () {
+      test("A simple true", () {
+        var scriptContent = 'let x = 5;';
+        var tokenizer = JackTokenizer(scriptContent);
+        expect(tokenizer.hasMoreTokens(), equals(true));
+      });
 
-    test('Ignore newlines', () {
-      var scriptContent = '\n';
-      var tokenizer = JackTokenizer(scriptContent);
-      expect(tokenizer.hasMoreTokens(), equals(false));
-    });
+      test("a symbol ;", () {
+        var scriptContent = ';';
+        var tokenizer = JackTokenizer(scriptContent);
+        expect(tokenizer.hasMoreTokens(), equals(true));
+      });
 
-    test('Ignore whitespace and newlines', () {
-      var scriptContent = '  \n';
-      var tokenizer = JackTokenizer(scriptContent);
-      expect(tokenizer.hasMoreTokens(), equals(false));
-    });
+      test("integer", () {
+        var scriptContent = '30';
+        var tokenizer = JackTokenizer(scriptContent);
+        expect(tokenizer.hasMoreTokens(), equals(true));
+      });
 
-    test('Token after new line and whitespace', () {
-      var scriptContent = '  \n let x = 5;';
-      var tokenizer = JackTokenizer(scriptContent);
-      expect(tokenizer.hasMoreTokens(), equals(true));
-    });
+      test("string constant", () {
+        var scriptContent = '"ha"';
+        var tokenizer = JackTokenizer(scriptContent);
+        expect(tokenizer.hasMoreTokens(), equals(true));
+      });
 
-    test('ignore // comment', () {
-      var scriptContent = '// comment \n';
-      var tokenizer = JackTokenizer(scriptContent);
-      expect(tokenizer.hasMoreTokens(), equals(false));
-    });
+      test("a keyword", () {
+        var scriptContent = 'field';
+        var tokenizer = JackTokenizer(scriptContent);
+        expect(tokenizer.hasMoreTokens(), equals(true));
+      });
 
-    test('code after // comment', () {
-      var scriptContent = '// comment\nlet x = 5;';
-      var tokenizer = JackTokenizer(scriptContent);
-      expect(tokenizer.hasMoreTokens(), equals(true));
-    });
-
-    test('ignore /* comment */', () {
-      var scriptContent = '/* comment */';
-      var tokenizer = JackTokenizer(scriptContent);
-      expect(tokenizer.hasMoreTokens(), equals(false));
-    });
-
-    test('code after /* comment */', () {
-      var scriptContent = '/* comment */let x = 5;';
-      var tokenizer = JackTokenizer(scriptContent);
-      expect(tokenizer.hasMoreTokens(), equals(true));
-    });
-
-    test('ignore /** comment */', () {
-      var scriptContent = '/** comment */';
-      var tokenizer = JackTokenizer(scriptContent);
-      expect(tokenizer.hasMoreTokens(), equals(false));
-    });
-
-    test('code after /** comment */', () {
-      var scriptContent = '/** comment */let x = 5;';
-      var tokenizer = JackTokenizer(scriptContent);
-      expect(tokenizer.hasMoreTokens(), equals(true));
+      test("an identifier", () {
+        var scriptContent = 'var_name';
+        var tokenizer = JackTokenizer(scriptContent);
+        expect(tokenizer.hasMoreTokens(), equals(true));
+      });
     });
   });
 }
